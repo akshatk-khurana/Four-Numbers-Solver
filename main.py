9d15a481c1d1ed1c0ec92302c280d36a0f7e17cf
Akshat Khurana <akshat.khurana@yahoo.com>
2024-07-12 16:43:31 +1000

    Finished the main solver file

--- main.py
+++ main.py
@@ -1,54 +1,54 @@
-pm = ['+', '-']
-td = ['*', '/']
-operators = pm+td
-triple_bracket_front = '^\([0-9][+*-/][0-9][+*-/][0-9]\)[+*-/][0-9]$'
-triple_bracket_back = '^[0-9][+*-/]\([0-9][+*-/][0-9][+*-/][0-9]\)$'
-double_bracket_back = '^[0-9][+*-/][0-9][+*-/]\([0-9][+*-/][0-9]\)$'
-double_bracket_front = '^\([0-9][+*-/][0-9]\)[+*-/][0-9][+*-/][0-9]$'
-two_bracket = '^\([0-9][+*-/][0-9]\)[+*-/]\([0-9][+*-/][0-9]\)$'
-middle_bracket = '^[0-9][+*-/]\([0-9][+*-/][0-9]\)[+*-/][0-9]$'
+from helper import operators, op_sorted, add_brackets, remove_brackets
 
-import re
 
-def op_sorted(operator, seq):
-    return f'{operator}'.join(sorted(seq.split(operator)))
+all_combos = []
+def find_combos(level, current, nums, target):
+    if level == 4:
+        for i in nums:
+            j = str(current)+str(i)
 
-def add_brackets(seq):
-    return [f'({seq[0:5]}){seq[5:]}', f'{seq[:2]}({seq[2:]})', f'({seq[0:3]}){seq[3]}({seq[4:]})', 
-            f'{seq[0:4]}({seq[4:]})', f'({seq[0:3]}){seq[3:]}', f'{seq[0:2]}({seq[2:5]}){seq[5:]}']
+            valid = True
 
-def remove_brackets(seq) -> list[str]:
-    if re.search(triple_bracket_front, seq):
-        if seq[7] in pm:
-            return seq[1:6]+seq[7:]
-    elif re.search(triple_bracket_back, seq):
-        if seq[1] == '-':
-            seq2 = seq[3:8].replace('-', 'p').replace('+', '-').replace('p', '+')
-            return seq[0:2]+seq2
-        elif seq[1] == '+':
-            return seq[0:2]+seq[3:8]
-    elif re.search(double_bracket_back, seq):
-        if seq[3] == '-':
-            seq2 = seq[5:8]
-            seq2 = seq2.replace('-', 'p').replace('+', '-').replace('p', '+')
-            return seq[0:4]+seq2
-        elif seq[3] == '+':
-            return seq[0:4]+seq[5:8]
-    elif re.search(double_bracket_front, seq):
-        if seq[5] in pm:
-            return seq[1:4]+seq[5]+seq[6:10]
-    elif re.search(two_bracket, seq):
-        if seq[5] in pm:
-           return seq[1:4]+seq[5]+seq[7:10] 
-    elif re.search(middle_bracket, seq):
-        if seq[1] in pm and seq[7] in pm:
-            if seq[1] == '-':
-                if seq[4] == '-':
-                    return seq[0:2]+seq[3]+'+'+seq[5]+seq[7:]
-                elif seq[4] == '+':
-                    return seq[0:2]+seq[3]+'-'+seq[5]+seq[7:]
-                else:
-                    return seq[0:2]+seq[3:6]+seq[7:]
-            elif seq[1] == '+':
-                return seq[0:2]+seq[3:6]+seq[7:]
-    return seq
+            if '/0' in j:
+                valid = False
+
+            if valid:
+                if eval(j) == float(target):
+                    exists = False
+                    ops = [current[1], current[3], current[5]]
+                    if ops == ['+']*3 or ops == ['*']*3:
+                        if op_sorted(ops[0], j) in all_combos:
+                            exists = True
+                    if not exists:
+                        if j not in all_combos:
+                            all_combos.append(j)
+
+                for l in add_brackets(j):
+                    removed = remove_brackets(l)
+                    try:
+                        if removed not in all_combos and eval(removed) == float(target):
+                            all_combos.append(removed)
+                    except:
+                        continue
+    else:
+        for i in nums:
+            curr_nums = nums.copy()
+            curr_nums.remove(i)
+            for j in operators:
+                k = str(current) + str(i) + j
+                find_combos(level+1, k, curr_nums, target)
+
+while True:
+    all_combos = []
+    target = int(input('Target to make: ', ))
+    numbers = list(input('Numbers: '))
+
+    find_combos(1, '', numbers, target)
+    if len(all_combos) == 0:
+        print('\nNo solutions found.')
+    else:
+        print('\nNumber of solutions found: ', len(all_combos))
+        print('Solutions found: ', *all_combos, sep='\n')
+
+    if input('Continue? [y/n]: ') == 'n':
+        break
